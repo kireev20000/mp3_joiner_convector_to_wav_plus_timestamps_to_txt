ce522c17423e2878e6b3f481959a31b749c5adb1
Alexsandr <kireev20000@mail.ru>
2023-05-15 21:56:37 +0700

    alpha release

---
+++ _mp3 to wavYT.bat
@@ -0,0 +1,2 @@
+python c:\EA\MP3_Joiner_TimeTips\mp3_merge+Timestamp.py
+
--- mp3_merge+Timestamp.py
+++ mp3_merge+Timestamp.py
@@ -31,13 +31,16 @@ for extension in extension_list:
         if mp3_tags.get('TAG', {}).get('grouping') is None:
             mp3_tag_grouping = ''
         else:
-            mp3_tag_grouping = mp3_tags['TAG']['grouping']
+            mp3_tag_grouping = '(' + mp3_tags['TAG']['grouping'] + ')'
+        if mp3_tags.get('TAG', {}).get('artist') is None:
+            mp3_tag_artist = ''
+        else:
+            mp3_tag_artist = mp3_tags['TAG']['artist'] + ' - '
 
         list_lenght.append(
                             str(start).split('.')[0] + ' Track ' +
-                            (mp3_tags['TAG']['track']).split('/')[0] + ' ' +
-                            mp3_tags['TAG']['title'] + '' +
-                            mp3_tag_grouping
+                            (mp3_tags['TAG']['track']).split('/')[0] + ' ' + mp3_tag_artist +
+                            mp3_tags['TAG']['title'] + ' ' + mp3_tag_grouping
         )
         start += parse_ts(end)
         combined += mp3_to_list
@@ -46,13 +49,13 @@ for extension in extension_list:
     combined.export(output_file_name, format="wav")
     print(*list_lenght, sep='\n')
 
-    with open("info.txt", 'w') as output:
+    with open("info.txt", 'w', encoding='utf-8') as output:
         for row in list_lenght:
             output.write(str(row) + '\n')
     break
 
 image = Image.open('folder.jpg')
-new_image = image.resize((720, 720))
+new_image = image.resize((1080, 1080))
 new_image.save('00.jpg')
 
 cmd_to_run = str(f'ffmpeg -loop 1 -r 1 -i "00.jpg" -vcodec vp8 -i "{output_file_name}" -acodec copy -shortest "{output_file_name[:len(output_file_name)-4]}.mkv"')
